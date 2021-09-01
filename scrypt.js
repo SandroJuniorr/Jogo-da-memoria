c0cb7c1a8b228a7f635d0219cbbb44ffb002da27
Sandro <sandrox377@gmail.com>
2021-09-01 17:39:05 -0300

    criando cartas

--- scrypt.js
+++ scrypt.js
@@ -1,5 +1,7 @@
 const front = "card_front";
 const back = "card_back";
+const Card = "card";
+
 let techs = [
     'bootstrap',
     'css',
@@ -19,11 +21,50 @@ starGame()
 function starGame(){
     cards = creatCardsfromtechs(techs);
     suffleCards(cards)
-    console.log(cards)
+    
 
+    initializeCards(cards)
+
+        
+
+}
+ function initializeCards(cards){
+    let cardBoard = document.getElementById("cardboard")
+
+    cards.forEach(card => {
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.dataset.Icon = card.icon
+        cardElement.classList.add(Card);
+        
+
+        cardFace(front, card , cardElement)
+        cardFace(back, card , cardElement)
+        cardElement.addEventListener('click' , flipperface)
+        cardBoard.appendChild(cardElement)
+    });
+   
+
+}
+function cardFace(face, card, Element){
+    let faceCard = document.createElement('div');
+    if(face === front){
+        faceCard.classList.add(front)
+        let facetech = document.createElement('img')
+        facetech.src = "./assets/images/" + card.icon + ".png"
+        faceCard.appendChild(facetech)
+
+    }else{
+        faceCard.classList.add(back)
+        faceCard.innerHTML = "&lt/&gt"
+    }
+    Element.appendChild(faceCard)
+    
+      
 
 
 }
+
 function suffleCards(cards){
     currentIndex = cards.length;
     randomIndex = 0;
@@ -64,4 +105,8 @@ function creatPairfromTech(tech){
 }
 function creatIDWithTech(tech){
     return tech + parseInt(Math.random() * 1000)
+}
+
+function flipperface(){
+    this.classList.add("flip")
 }
